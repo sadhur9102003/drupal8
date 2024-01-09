965842a465ce10d740f0521ff2473d034ed16b28
sadhur9102003 <sadhursadhur19@gmail.com>
2024-01-09 16:33:48 +0530

    tas1-adding libraries and twig file debugging

---
+++ sites/default/files/php/twig/659d27ad355f6_region--header.html.twig_q6bCFxdtPTWKFuvulIHo6iDFI/qCWZ3IxjOsgYiCvEnm_9swCGVsCqcFEDduMlYRabLOU.php
@@ -0,0 +1,129 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/cus_boo/templates/system/region--header.html.twig */
+class __TwigTemplate_53268652512f236c13ab100891c3733bdfd6f8406d15bb27a4ebe7f3927925f4 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 18, "if" => 24];
+        $filters = ["clean_class" => 20, "escape" => 25];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 18
+        $context["classes"] = [0 => "region", 1 => ("region-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 20
+($context["region"] ?? null)))), 2 => "container-fluid d-flex align-items-center justify-content-between"];
+        // line 24
+        if (($context["content"] ?? null)) {
+            // line 25
+            echo "  <div";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+            echo ">
+  ";
+            // line 27
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "html", null, true);
+            echo "
+  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/cus_boo/templates/system/region--header.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  65 => 27,  60 => 25,  58 => 24,  56 => 20,  55 => 18,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a region.
+ *
+ * Available variables:
+ * - content: The content for this region, typically blocks.
+ * - attributes: HTML attributes for the region div.
+ * - region: The name of the region variable as defined in the theme's
+ *   .info.yml file.
+ *
+ * @ingroup templates
+ *
+ * @see template_preprocess_region()
+ */
+#}
+{%
+  set classes = [
+    'region',
+    'region-' ~ region|clean_class,
+    'container-fluid d-flex align-items-center justify-content-between'
+  ]
+%}
+{% if content %}
+  <div{{ attributes.addClass(classes) }}>
+  {#{ dd(content) }#}
+    {{ content }}
+  </div>
+{% endif %}
+", "themes/custom/cus_boo/templates/system/region--header.html.twig", "/var/www/html/drupal/themes/custom/cus_boo/templates/system/region--header.html.twig");
+    }
+}
